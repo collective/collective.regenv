0c2438e6c84429a52fea3038023fc5840f05df11
Mauro Amico <mauro.amico@gmail.com>
2022-12-27 11:56:22 +0100

    feat: check yaml

--- src/collective/regenv/__init__.py
+++ src/collective/regenv/__init__.py
@@ -13,5 +13,11 @@ _ = MessageFactory("collective.regenv")
 if os.environ.get("PLONE_REGISTRY_YAML"):
     with open(os.environ.get("PLONE_REGISTRY_YAML")) as fh:
         registry = yaml.safe_load(fh)
-    apply_plone_registry_monkey(registry)
-    apply_propertymanager_monkey(registry)
+    if not isinstance(registry, dict):
+        raise ValueError(
+            "PLONE_REGISTRY_YAML must point to a YAML file with a dictionary"
+        )
+    if registry.keys().filter(lambda x: x.endswith("portal_registry")):
+        apply_plone_registry_monkey(registry)
+    if registry.keys().filter(lambda x: not x.endswith("portal_registry")):
+        apply_propertymanager_monkey(registry)
