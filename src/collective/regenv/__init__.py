475d4e9ec9fe1e0f1d5dd6ec7b292bcf8e0f08ed
Mauro Amico <mauro.amico@gmail.com>
2022-12-27 12:16:50 +0100

    fix: initialize

--- src/collective/regenv/__init__.py
+++ src/collective/regenv/__init__.py
@@ -17,7 +17,7 @@ if os.environ.get("PLONE_REGISTRY_YAML"):
         raise ValueError(
             "PLONE_REGISTRY_YAML must point to a YAML file with a dictionary"
         )
-    if registry.keys().filter(lambda x: x.endswith("portal_registry")):
+    if list(filter(lambda x: x.endswith("portal_registry"), registry.keys())):
         apply_plone_registry_monkey(registry)
-    if registry.keys().filter(lambda x: not x.endswith("portal_registry")):
+    if list(filter(lambda x: not x.endswith("portal_registry"), registry.keys())):
         apply_propertymanager_monkey(registry)
