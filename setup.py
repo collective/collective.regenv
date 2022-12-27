b16eb6399588c335e744b2a734618d687ebf7812
Mauro Amico <mauro.amico@gmail.com>
2022-12-27 12:17:47 +0100

    Back to development: 1.0.0a3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.regenv',
-    version='1.0.0a2',
+    version='1.0.0a3.dev0',
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
