7bc575a5259411fe80adf7331f3195cda50b3954
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-11-15 10:41:10 +0100

    feat: add an option to get the values from a yaml content (#4)
    * feat: add an option to get the values from a yaml content
    
    * improve error message
    
    * ci: removed py27, make env mutually exclusive
    
    ---------
    
    Co-authored-by: Mauro Amico <mauro.amico@gmail.com>

--- src/collective/regenv/__init__.py
+++ src/collective/regenv/__init__.py
@@ -10,12 +10,19 @@ import yaml
 _ = MessageFactory("collective.regenv")
 
 
+registry = None
+
 if os.environ.get("PLONE_REGISTRY_YAML"):
     with open(os.environ.get("PLONE_REGISTRY_YAML")) as fh:
         registry = yaml.safe_load(fh)
+
+elif os.environ.get("PLONE_REGISTRY_YAML_CONTENT"):
+    registry = yaml.safe_load(os.environ.get("PLONE_REGISTRY_YAML_CONTENT"))
+
+if registry is not None:
     if not isinstance(registry, dict):
         raise ValueError(
-            "PLONE_REGISTRY_YAML must point to a YAML file with a dictionary"
+            "PLONE_REGISTRY_YAML must point to a YAML file with a dictionary. Alternatively PLONE_REGISTRY_YAML_CONTENT must have a dictionary in YAML format"
         )
     if list(filter(lambda x: x.endswith("portal_registry"), registry.keys())):
         apply_plone_registry_monkey(registry)
